ddc7c3ffabac4838aa17e88431a60c66f43184fd
FeliCarlomagno <feliciano.carlomagno@libero.it>
2023-02-07 17:15:09 +0100

    Add books

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -1,14 +1,14 @@
 import { Card, Button, Container, Row } from "react-bootstrap";
-import book from "../../src/books";
+import book from "../../src/books/fantasy.json";
 
 const AllTheBooks = () => (
   <Container>
     <Row>
       {book.map((book) => (
-        <Card style={{ width: "18rem" }} className="col">
-          <Card.Img variant="top" src={book.cover} />
+        <Card style={{ width: "18rem" }} className="col-12">
+          <Card.Img variant="top" src={book.img} />
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
+            <Card.Title>{book.title}</Card.Title>
             <Card.Text>
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
